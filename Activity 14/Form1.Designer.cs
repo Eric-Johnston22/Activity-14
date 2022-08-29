113166ff4fc7f89c9cc18cb5ef1b5dd5141302b0
Eric-Johnston <eric.mjohnston@protonmail.com>
2022-08-29 02:31:02 -0400

    Add project files.

---
+++ Activity 14/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace Activity_14
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox_images = new System.Windows.Forms.ListBox();
+            this.checkBox_name = new System.Windows.Forms.CheckBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btn_display = new System.Windows.Forms.Button();
+            this.checkBox_date = new System.Windows.Forms.CheckBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.radio_lightmode = new System.Windows.Forms.RadioButton();
+            this.radio_darkmode = new System.Windows.Forms.RadioButton();
+            this.lbl_name = new System.Windows.Forms.Label();
+            this.lbl_date = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBox_images
+            // 
+            this.listBox_images.FormattingEnabled = true;
+            this.listBox_images.Items.AddRange(new object[] {
+            "Dragon",
+            "Planet",
+            "Rooster"});
+            this.listBox_images.Location = new System.Drawing.Point(48, 42);
+            this.listBox_images.Name = "listBox_images";
+            this.listBox_images.Size = new System.Drawing.Size(139, 121);
+            this.listBox_images.TabIndex = 0;
+            // 
+            // checkBox_name
+            // 
+            this.checkBox_name.AutoSize = true;
+            this.checkBox_name.Location = new System.Drawing.Point(21, 15);
+            this.checkBox_name.Name = "checkBox_name";
+            this.checkBox_name.Size = new System.Drawing.Size(54, 17);
+            this.checkBox_name.TabIndex = 1;
+            this.checkBox_name.Text = "Name";
+            this.checkBox_name.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(331, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(446, 370);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btn_display
+            // 
+            this.btn_display.Location = new System.Drawing.Point(48, 372);
+            this.btn_display.Name = "btn_display";
+            this.btn_display.Size = new System.Drawing.Size(107, 40);
+            this.btn_display.TabIndex = 3;
+            this.btn_display.Text = "Show Image";
+            this.btn_display.UseVisualStyleBackColor = true;
+            this.btn_display.Click += new System.EventHandler(this.btn_display_Click);
+            // 
+            // checkBox_date
+            // 
+            this.checkBox_date.AutoSize = true;
+            this.checkBox_date.Location = new System.Drawing.Point(21, 39);
+            this.checkBox_date.Name = "checkBox_date";
+            this.checkBox_date.Size = new System.Drawing.Size(49, 17);
+            this.checkBox_date.TabIndex = 4;
+            this.checkBox_date.Text = "Date";
+            this.checkBox_date.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.checkBox_name);
+            this.panel1.Controls.Add(this.checkBox_date);
+            this.panel1.Location = new System.Drawing.Point(48, 279);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(121, 68);
+            this.panel1.TabIndex = 5;
+            // 
+            // radio_lightmode
+            // 
+            this.radio_lightmode.AutoSize = true;
+            this.radio_lightmode.Location = new System.Drawing.Point(48, 186);
+            this.radio_lightmode.Name = "radio_lightmode";
+            this.radio_lightmode.Size = new System.Drawing.Size(77, 17);
+            this.radio_lightmode.TabIndex = 6;
+            this.radio_lightmode.TabStop = true;
+            this.radio_lightmode.Text = "Light mode";
+            this.radio_lightmode.UseVisualStyleBackColor = true;
+            // 
+            // radio_darkmode
+            // 
+            this.radio_darkmode.AutoSize = true;
+            this.radio_darkmode.Location = new System.Drawing.Point(48, 210);
+            this.radio_darkmode.Name = "radio_darkmode";
+            this.radio_darkmode.Size = new System.Drawing.Size(78, 17);
+            this.radio_darkmode.TabIndex = 7;
+            this.radio_darkmode.TabStop = true;
+            this.radio_darkmode.Text = "Dark Mode";
+            this.radio_darkmode.UseVisualStyleBackColor = true;
+            // 
+            // lbl_name
+            // 
+            this.lbl_name.AutoSize = true;
+            this.lbl_name.Location = new System.Drawing.Point(464, 398);
+            this.lbl_name.Name = "lbl_name";
+            this.lbl_name.Size = new System.Drawing.Size(0, 13);
+            this.lbl_name.TabIndex = 8;
+            // 
+            // lbl_date
+            // 
+            this.lbl_date.AutoSize = true;
+            this.lbl_date.Location = new System.Drawing.Point(610, 398);
+            this.lbl_date.Name = "lbl_date";
+            this.lbl_date.Size = new System.Drawing.Size(0, 13);
+            this.lbl_date.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbl_date);
+            this.Controls.Add(this.lbl_name);
+            this.Controls.Add(this.radio_darkmode);
+            this.Controls.Add(this.radio_lightmode);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btn_display);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.listBox_images);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox_images;
+        private System.Windows.Forms.CheckBox checkBox_name;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btn_display;
+        private System.Windows.Forms.CheckBox checkBox_date;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RadioButton radio_lightmode;
+        private System.Windows.Forms.RadioButton radio_darkmode;
+        private System.Windows.Forms.Label lbl_name;
+        private System.Windows.Forms.Label lbl_date;
+    }
+}
+
---
+++ Activity 14/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Activity_14
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_display_Click(object sender, EventArgs e)
+        {
+            // Get image string from listbox
+            string selectedImage = listBox_images.SelectedItem.ToString();
+
+            // Display image to picture box
+            this.pictureBox1.Image = Image.FromFile(@"C:\Users\ejohn\source\repos\Grand Canyon University\CST-150\Activity 14\images\" + selectedImage + ".jpg");
+
+            // Change form colors
+            if (radio_lightmode.Checked)
+            {
+                this.BackColor = SystemColors.Control;
+                this.ForeColor = SystemColors.ControlText;
+            }
+            if (radio_darkmode.Checked)
+            {
+                this.BackColor = SystemColors.Desktop;
+                this.ForeColor = SystemColors.ButtonShadow;
+            }
+
+            // Display name of image
+            if(checkBox_name.Checked)
+            {
+                lbl_name.Text = selectedImage + ".jpg";
+            }
+            // Display date
+            if(checkBox_date.Checked)
+            {
+                lbl_date.Text = DateTime.Now.ToString();
+            }
+        }
+    }
+}
